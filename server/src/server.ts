db7c058dc35becc1034f0b6d5a09a6d13efbd3f5
Ricardo Costa <elberricardo2001@gmail.com>
2023-01-31 12:14:32 -0100

    finiching routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,12 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 
-const prisma = new PrismaClient();
-
 app.register(cors);
-
-app.get("/", async () => {
-  const habit = await prisma.habit.findMany();
-
-  return habit;
-});
+app.register(appRoutes);
 
 app
   .listen({
