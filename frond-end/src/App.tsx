a6aefb36a90cd0f772b5bd74b75df4e626f52d50
Ricardo Costa <elberricardo2001@gmail.com>
2023-02-02 11:21:09 -0100

    finish front-end

--- frond-end/src/App.tsx
+++ frond-end/src/App.tsx
@@ -1,7 +1,16 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./style/global.css";
 
 function App() {
-  return <div className="text-2xl font-bold">hello wold</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl p-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
 }
 
 export default App;
