68d85db71895766081f134b75c84133f12329f80
Ricardo-Robot <elberricardo2001@gmail.com>
2023-02-27 15:24:54 -0100

    Create New Habit Screen

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { View, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import Logo from "../assets/logo.svg";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -12,6 +15,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate("newHabit")}
       >
         <Feather name="plus" size={20} color={colors.violet[500]} />
         <Text className="text-white ml-3 font-semibold text-base">New</Text>
